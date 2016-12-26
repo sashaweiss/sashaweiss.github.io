c050d349a8d860292f1a442ee6770927340f9766
sashaweiss <sasha.weiss@u.northwestern.edu>
2016-12-26 15:21:49 -0600

    Fixed banner vh and JS trigger only on mobile

--- assets/js/mobile_url_resize.js
+++ assets/js/mobile_url_resize.js
@@ -1,10 +1,14 @@
 var background_div = jQuery(".background-image");
 
-var og_window_height = $(window).height();
+// Set the height of the background image to be 60px more than the height of
+// the window, so that it doesn't jump on resize when the URL bar on mobile
+// disappears.
 
 // $(window).resize(resizeBackground);
-function resizeBackground() {
+function resizeBackgroundForMobile() {
   background_div.height($(window).height() + 60);
 }
 
-resizeBackground();
+if ($(window).width() < 480) {
+  resizeBackgroundForMobile();
+}
