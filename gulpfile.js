74f8ab122b5b4df7785bc8b74a909a745f2633ac
sashaweiss <sasha.weiss@getbraintree.com>
2017-06-26 12:48:45 -0700

    watch src and assets for changes run gulp automatically

--- gulpfile.js
+++ gulpfile.js
@@ -24,4 +24,10 @@ gulp.task('about', function () {
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['index', 'about']);
+console.log('Watching src/ and assets/ for changes...');
+var tasks = ['index', 'about'];
+var watcher = gulp.watch(['src/**/*', 'assets/**/*'], tasks);
+watcher.on('change', function (event) {
+	console.log(`File ${event.path} was ${event.type}, running tasks: ${tasks.map((t) => `"${t}"`).join(', ')}...`);
+});
+
